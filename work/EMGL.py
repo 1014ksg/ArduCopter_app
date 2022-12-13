8edfeb2a78f7e05aaf0fbd3705e0b505de7e659e
1014ksg <1014ksg1@keio.jp>
2022-12-14 08:46:46 +0900

    fix:bug

--- work/EMGL.py
+++ work/EMGL.py
@@ -24,24 +24,47 @@ vehicle.commands.next=0
 
 cmds = vehicle.commands
 cmds.clear()
-lat1 = 35.8782484
-lon1 = 140.3382839
-altitude = 30.0
+
+lat1 = 35.8791362
+lon1 = 140.3383309
+altitude = 10.0
+#add dummy waypoint at point (lets us know when have reached destination)
 cmds.add(Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
     0, 0, 0, 0, 0, 0,
     lat1, lon1, altitude))
 
-lat2 = 35.8785484
-lon2 = 140.3382539
+cmds.add(Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+    0, 0, 0, 0, 0, 0,
+    lat1, lon1, altitude))
 
+lat2 = 35.8782669
+lon2 = 140.3374135
 cmds.add(Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
     0, 0, 0, 0, 0, 0,
     lat2, lon2, altitude))
 
-#add dummy waypoint "5" at point 4 (lets us know when have reached destination)
+lat3 = 35.8776801
+lon3 = 140.3382450
 cmds.add(Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
     0, 0, 0, 0, 0, 0,
-    lat2, lon2, altitude))
+    lat3, lon3, altitude))
+
+lat4 = 35.8779757
+lon4 = 140.3395325
+cmds.add(Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+    0, 0, 0, 0, 0, 0,
+    lat4, lon4, altitude))
+
+lat5 = 35.8790015
+lon5 = 140.3405249
+cmds.add(Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+    0, 0, 0, 0, 0, 0,
+    lat4, lon4, altitude))
+
+#add dummy waypoint at point (lets us know when have reached destination)
+cmds.add(Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+    0, 0, 0, 0, 0, 0,
+    lat5, lon5, altitude))
 
 cmds.upload()
 
@@ -52,7 +75,6 @@ vehicle.commands.next=0
 # Set mode to AUTO to start mission
 vehicle.mode = VehicleMode("AUTO")
 
-time.sleep(10)
+time.sleep(20) # 奥側で着地
+# time.sleep(50) # 中央で着地
 vehicle.parameters['SIM_BATT_VOLTAGE']= 9.7
-
-
